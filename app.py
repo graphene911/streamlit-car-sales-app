869e660810026e5e3a056586c39769c14c43b9d8
graphene911 <graphene911@gmail.com>
2022-05-29 11:09:58 +0900

    add

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
+from app_eda import run_eda
+from app_home import run_home
+from app_ml import run_ml
 
 
 
 
 
 def main() :
-    pass
+    st.title('고객 차량 구매가능금액 예측 및 추천 앱')
+
+    menu = ['Home', 'EDA', 'ML']
+    choice = st.sidebar.selectbox('메뉴 선택', menu)
+
+    if choice == menu[0] :
+        run_home()
+    elif choice == menu[1] :
+        run_eda()
+    elif choice == menu[2] :
+        run_ml()
 
 
 
